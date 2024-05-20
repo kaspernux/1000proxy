788fafa30d46b984c18e99605c1a554dc083faa3
root <root@x.1000proxy.bot>
2024-05-20 04:54:46 +0200

    Initial Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Create an admin user
+        User::factory()->create([
+            'name' => 'Test Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('Admin123'),
+        ]);
 
+        // Create a regular user
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'user@email.com',
+            'password' => Hash::make('Userd123'),
         ]);
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import laravel, { refreshPaths } from 'laravel-vite-plugin'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default defineConfig( {
     plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: [
-                ...refreshPaths,
-                'app/Filament/**',
-                'app/Forms/Components/**',
-                'app/Livewire/**',
-                'app/Infolists/Components/**',
-                'app/Providers/Filament/**',
-                'app/Tables/Columns/**',
-            ],
-        }),
+        laravel( {
+            input: [ 'resources/css/app.css', 'resources/js/app.js' ],
+            refresh: true,
+        } ),
+        vue(),
     ],
-})
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm.js',
+            '@': `${ process.cwd() }/resources/js` // Corrected path for '@' alias
+        }
+    }
+} );
