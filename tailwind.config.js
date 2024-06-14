8f4831f5a63a68d10ef2a1861c724f539be557e0
Kaspernux <ouangapou@gmail.com>
2024-06-14 19:26:22 +0200

    Home Page design 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,19 @@ export default {
 
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'dark-green': '#004B23',
+                'dark-green-600': '#006400',
+                'dark-green-500': '#007200',
+                'dark-green-400': '#008000',
+                'accent-green': '#38B000',
+                'light-green': '#70E000',
+                'light-green-200': '#9EF01A',
+                'light-green-100': '#CCFF33',
+                'light-dark': '#2D2D2D'
+            }
+        },
     },
     plugins: [
       require('preline/plugin'),
